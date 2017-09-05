d4797e8ef24d037043447e61436f1b7786b26f08
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2017-09-05 16:38:25 +0530

    Create eo.c

---
+++ eo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter a num");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("/n the num is even");
+}
+else
+printf("/n the num is odd");
+}
